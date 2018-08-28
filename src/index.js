f097b2678b90f78f2c997fbc97d2d397fd2fd308
Minchan <mc.kim@tesnine.com>
2018-08-28 17:46:21 +0900

    Add express api log

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
+var logger = require('morgan');
 
 
-
+app.use(logger(function (tokens, req, res) {
+  var body = req.body;
+  delete body.password;
+  delete body.verify;
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms /',
+    new Date().toLocaleString(), ' / ',
+    JSON.stringify(body)
+  ].join(' ')
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
